e9a8fd694045d7d2e1ecfca55905eaf7b0653d34
Nat <bnat1@umbc.edu>
2017-03-29 21:41:18 -0400

    generated correct randome numbers for tests

--- driver.cpp
+++ driver.cpp
@@ -1,64 +1,52 @@
 #include <iostream>
-#include <cstdlib>
+#include <stdlib.h>
 #include "posint.h"
 using namespace std;
 
 int main() {
 
-  // Set base to 16^3
+  // Set base to 16^4
   // This causes numbers to display in hex.  
   // However, base must be no greater than 2^15 when
   // using 32-bit integers, so we can only fit 3 hex 
   // digits (or 12 bits) per 32-bit int.  It is more
   // space-efficient to use the default, but then the
   // numbers will print in binary.
-  
-  PosInt::setBase(10);
-  
-  // PosInt::setBase(10, 4);
+  // PosInt::setBase(16, 3);
+	PosInt::setBase(10, 1);
+	srand(time(NULL));
 
-  //64 * 16
-  // PosInt test1a(64);
-  // PosInt test1b(16);
-  // test1a.fastMul(test1b);
-  // test1a.print(cout);
-  
-  //2148 ^ 2
-  PosInt test2a(32540);
-  PosInt test2b(1423458);
-  test2a.fastMul(test2b);
-  test2a.print_array(cout);
-  test2a.print(cout);
+	int trialsPerBit = 2;
+	int maxBits = 64;
 
+	PosInt one(1);
+	PosInt two(2);
+	PosInt lowerBound(1);
+	PosInt upperBound(2);
+	PosInt upperLowerDiff(upperBound);
+	upperLowerDiff.sub(lowerBound);
 
+	PosInt mulTester;
+	PosInt fastMulTester;
+	PosInt x;
+	PosInt y;
+	for(int i = 1; i < maxBits; ++i){
+		cout << "bits: " << i << endl;
+		cout << "lower bound: " << lowerBound << endl;
+		cout << "upper bound: " << upperBound << endl;			
+		for(int j = 0; j < trialsPerBit; ++j){
+			x.rand(upperLowerDiff);
+			x.add(lowerBound);
+			y.rand(upperLowerDiff);
+			y.add(lowerBound);
+			cout << "x: " << x << endl;
+			cout << "y: " << y << endl;
+		}
+		lowerBound.set(upperBound);
+		upperBound.mul(two);
+		upperLowerDiff.set(upperBound);
+		upperLowerDiff.sub(lowerBound);
+	}
 
-  // x = 2^128
-  // x.pow(y);
-
-  // z = random number between 0 and 2^128 - 1
-  //  srand(0);
-  // srand(time(NULL));
-  // PosInt z;
-  // z.rand(x);
-
-  // cout << "x = 2^128 = " << x << endl;
-  // cout << "x         = ";
-  // x.print_array(cout);
-  // cout << endl << endl;
-
-  // cout << "z = rand(x) = " << z << endl;
-  // cout << "z           = ";
-  // z.print_array(cout);
-  // cout << endl << endl;
-
-  // // z = z^2
-  // z.mul(z);
-
-  // cout << "z^2 = " << z << endl;
-
-  // // z = z mod x
-  // z.mod(x);
-
-  // cout << "z^2 mod x = " << z << endl;
 }
   
